b9f49cc7f3771e2af404bc58985d1189d44d8df2
iamrajendra <rajendrarajaram@hotmail.com>
2020-10-28 21:49:35 +0530

    New Year Chaos
     Time complexity problem.
    
    Please enter the commit message for your changes. Lines starting

---
+++ src/MinimumBribe.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class MinimumBribe {
+    static void minimumBribes(int[] queue) {
+        boolean chaotic = false;
+        int bribes = 0;
+        int temp;
+        for (int i = 0; i < queue.length; i++) {
+
+            if (queue[i] - (i + 1) > 2) {
+                System.out.println("Too chaotic");
+                return;
+            }
+            for (int j = 0; j < i; j++) {
+                if (queue[j] > queue[i]) {
+                    bribes++;
+                    temp  = queue[j];
+                    queue[j] = queue[i] ;
+                    queue[i] = temp;
+
+                    }
+            }
+
+        }
+         if (bribes!=0)  System.out.println(bribes);
+
+    }
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            int[] q = new int[n];
+
+            String[] qItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < n; i++) {
+                int qItem = Integer.parseInt(qItems[i]);
+                q[i] = qItem;
+            }
+
+            minimumBribes(q);
+        }
+
+        scanner.close();
+    }
+}
